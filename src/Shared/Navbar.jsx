898b69c4f966a156a1fd2c81a090b58362234298
ornobaadi <aadi4789@gmail.com>
2025-04-09 23:07:21 +0600

    Shadcn Magicui shiny button

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { ShimmerButton } from '../components/magicui/shimmer-button';
 
 const Navbar = () => {
     return (
-        <div className='w-7/12 mx-auto'>
-            <div className="navbar bg-base-300 shadow-sm rounded-full my-2">
+        <div className="w-7/12 mx-auto">
+            <div className="navbar bg-base-300 shadow-sm rounded-lg my-2">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl rounded-full">TickyTask</a>
+                    <a className="btn btn-ghost text-xl rounded-lg">TickyTask</a>
                 </div>
                 <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1">
-                        <button className="btn btn-soft btn-primary rounded-full">Login</button>
+                    <ul className="menu menu-horizontal px-2">
+                        <ShimmerButton rippleColor="173, 216, 230" className="btn rounded-lg">Login</ShimmerButton>
                     </ul>
                 </div>
             </div>
